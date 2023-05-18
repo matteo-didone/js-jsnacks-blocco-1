327e3f488da211264696f548729b92a4307f9a4f
matteo-didone <matteo.dido9@gmail.com>
2023-05-18 17:33:50 +0200

    Printing the digit sum to the page

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,6 +1,16 @@
 //Ask the user for a four-digit number 
 let fourDigitNumber = prompt("Please provide a four-digit number: ");
 
+// Create the <p> elements for number paragraph 
+let fourDigitNumberParagraph = document.createElement('p');
+fourDigitNumberParagraph.id = 'four-digit-paragraph';
+
+// Get the container div
+const fourDigitSumContainer = document.querySelector('.four-digit-sum-container');
+
+// Append the <p> elements to the container
+fourDigitSumContainer.appendChild(fourDigitNumberParagraph);
+
 //Check if the number is a four-digit number
 if (fourDigitNumber.length !== 4) 
 {
@@ -8,8 +18,6 @@ if (fourDigitNumber.length !== 4)
 }
 
 //If the number is a four-digit number, add each digit to an array
-
-
 //Create an empty array
 const digit = [];
 
@@ -31,4 +39,7 @@ for(let i = 0; i < digit.length; i++)
 }
 
 //Print the sum of all the digits in the array
-console.log(sumOfArrayElements);
+console.log("The sum of the digits you provided: " + sumOfArrayElements);
+
+//Print the sum of all the digits in the array in the <p> element
+fourDigitNumberParagraph.innerHTML = "The sum of the digits you provided: " + sumOfArrayElements;
